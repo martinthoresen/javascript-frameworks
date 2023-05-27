104ed4badd29dbdf5574353582fa8e38a0032c84
martinthoresen <87776525+martinthoresen@users.noreply.github.com>
2023-05-27 21:25:31 +0200

    Install react-router-dom, Create Layout component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer() {
+  <div>
+    <p>Copyright &copy; 2023 SwiftShop</p>
+  </div>;
+}
+
+export default Footer;
---
+++ src/components/Header/Nav/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/products">Products</Link>
+        </li>
+        <li>
+          <Link to="/cart">Shopping Cart</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
