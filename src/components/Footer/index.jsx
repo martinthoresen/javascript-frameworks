ad13b638b8f5eaea32c8ac74df93088b53242a4f
martinthoresen <87776525+martinthoresen@users.noreply.github.com>
2023-06-07 17:53:58 +0200

    Fixed footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className=" bg-dark navbar">
+    <footer className=" bg-dark navbar mt-5">
       <p className="text-white m-auto p-4">Copyright &copy; 2023 SwiftShop</p>
     </footer>
   );
