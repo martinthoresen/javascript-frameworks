f8bb72a169bb0930a7c886ceffa0a09b280bf144
martinthoresen <87776525+martinthoresen@users.noreply.github.com>
2023-06-11 21:39:18 +0200

    Update cartTotal.js

--- src/components/Utility/cartTotal.js
+++ src/components/Utility/cartTotal.js
@@ -1,6 +1,6 @@
 function cartTotal(data) {
   const initialValue = 0;
   const total = data.reduce((accumulator, current) => accumulator + current.discountedPrice * current.quantity, initialValue);
-  return "$" + total;
+  return "$" + total.toFixed(2);
 }
 export default cartTotal;
